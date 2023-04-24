8e91a98247fffe28dee1e7ea3f07f4f71b5b6b53
Alper Kantarci <alper.kantarci@sechard.com>
2023-04-24 21:04:35 +0200

    perf(astro-compress): Install astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
-      config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
     }),
+    compress(),
   ],
   site: 'https://alperk.dev',
 });

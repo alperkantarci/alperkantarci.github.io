4d9f110e4f34dd97ee25c7e5e59b7e965f239e0e
Alper Kantarci <alper.kantarci@sechard.com>
2023-04-25 01:02:37 +0200

    refactor: Update youtube link

--- src/constants/social-links.ts
+++ src/constants/social-links.ts
@@ -3,6 +3,6 @@ const mailto = 'mailto:alper.kantarci@hotmail.com';
 const github = 'https://github.com/alperkantarci';
 const linkedin = 'https://linkedin.com/in/alperkantarci';
 const instagram = 'https://instagram.com/alperkantrc';
-const youtube = 'https://www.youtube.com/@alperkantarci7667/videos';
+const youtube = 'https://www.youtube.com/@alperkantarci';
 
 export { mail, mailto, github, linkedin, instagram, youtube };

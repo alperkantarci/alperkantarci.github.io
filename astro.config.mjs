6c7781d967d4890525666683d51514edbc2dbc72
Alper Kantarci <alper.kantarci@sechard.com>
2023-04-24 18:57:51 +0200

    style(scrollbar): Improve scrollbar style

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+  ],
   site: 'https://alperk.dev',
 });

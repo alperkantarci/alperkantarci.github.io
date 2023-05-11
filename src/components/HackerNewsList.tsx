4382bef5084b42816822b8fb314980ee8760b4ba
Alper Kantarci <alper.kantarci@sechard.com>
2023-05-11 14:01:29 +0200

    style(hacker-news-list): Add x margins to list header

--- src/components/HackerNewsList.tsx
+++ src/components/HackerNewsList.tsx
@@ -52,7 +52,9 @@ const HackerNewsList = () => {
   return (
     <>
       <div
-        className={'flex justify-between items-center gap-4 text-sm font-bold sticky bg-zinc-950 bg-opacity-60 top-0 backdrop-blur-xl py-3'}
+        className={
+          'flex justify-between items-center gap-4 text-sm font-bold sticky bg-zinc-950 bg-opacity-60 top-0 backdrop-blur-xl py-3 -mx-5 px-5 rounded-lg'
+        }
       >
         <ul className={'hidden sm:flex gap-3'}>
           <li>

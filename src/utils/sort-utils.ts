684b382df586c85d345a0c8ff4d938ce33ce94c2
Alper Kantarci <alper.kantarci@hotmail.com>
2025-01-20 23:13:56 +0100

    refactor: Update sort-utils function to support "writings" collection entry sorting

--- src/utils/sort-utils.ts
+++ src/utils/sort-utils.ts
@@ -1,11 +1,13 @@
-function byDate(a: any, b: any) {
-  const aDate = new Date(a.slug || a.createdAt || a.frontmatter?.createdAt);
-  const bDate = new Date(b.slug || b.createdAt || b.frontmatter?.createdAt);
+import type { CollectionEntry } from "astro:content";
 
-  if (aDate < bDate) {
+function byDate(
+  a: CollectionEntry<"writings">,
+  b: CollectionEntry<"writings">
+) {
+  if (a.data.createdAt < b.data.createdAt) {
     return 1;
   }
-  if (aDate > bDate) {
+  if (a.data.createdAt > b.data.createdAt) {
     return -1;
   }
   return 0;
